d3519fa34e5d54e4e8478dfa7969f3c0a6b1badc
Carolina Daniel <carolina.daniel@alumni.usp.br>
2021-06-11 12:25:10 -0300

    Delete project01_Shell01 directory

--- project01_Shell01/ex02/find_sh.sh
+++
@@ -1,1 +0,0 @@
-find . -name "*.sh" | rev | cut -d "/" -f 1 | rev | cut -d "." -f 1
--- project01_Shell01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | grep -v "#" | sed -n 'n;p' | cut -d ":" -f 1 | rev | sort -r | awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | paste -s -d "," | sed 's/,/, /g' | sed 's/$/./' | tr -d '\n'
